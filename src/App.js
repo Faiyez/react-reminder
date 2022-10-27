5b38e5cf075082b89f668c0ea84842dcc5382fe4
Faiyez <smfnoor@mun.ca>
2022-10-27 10:37:23 -0230

    added reminder css

--- src/App.js
+++ src/App.js
@@ -24,10 +24,16 @@ const App = () => {
     const deleteTask = (id) =>{
       setTasks(tasks.filter((task) => task.id !== id))  
     }
+    const toggleReminder = (id) =>{
+      setTasks(tasks.map((task) => 
+      task.id === id ? {
+        ...task, reminder: !task.reminder}: task))
+    }
   return (
     <div className = 'container'>
       <Header />
-      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete = {deleteTask}/>)
+      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete = {deleteTask}
+      onToggle = {toggleReminder}/>)
       : ('Empty Tasks')}
     </div>
   )
--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,7 +1,10 @@
 import { FaTimes } from 'react-icons/fa'
-const Task = ({task, onDelete}) => {
+const Task = ({task, onDelete, onToggle}) => {
   return (
-    <div className='task'>
+    // class name task will stay anyway reminder will come for condition
+    <div className={`task ${task.reminder ? 'reminder':''}`} 
+    onDoubleClick = {() => onToggle(task.id)
+    }>
         <h3>{task.text} <FaTimes style={{
             color: 'orange',
             cursor: 'pointer'
