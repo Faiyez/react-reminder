6ea12469efa7461abcca218533250c1fe09f2159
Faiyez <smfnoor@mun.ca>
2022-11-02 11:22:41 -0230

    added functionalities

--- src/Components/AddTask.js
+++ src/Components/AddTask.js
@@ -1,5 +1,9 @@
-
+import { useState } from 'react'
 const AddTask = () => {
+  // Initial value = text and changed value setText
+  const [text, setText] = useState('')
+  const [day, setDay] = useState('')
+  const [reminder, setReminder] = useState('')
   return (
     <form className ='add-form'>
         <div className='form-control form-control-check'>
@@ -9,8 +13,14 @@ const AddTask = () => {
         </div>
         <div className='form-control form-control-check'>
             <label>Enter Date:</label>
-            <input type='text' placeholder=
-            'Enter Date and Time'></input>
+            <input 
+            type='text' 
+            placeholder='Enter Date and Time'
+            value={text} // Value is set to dynamic value text
+            onChange = {(e) => setText(e.target.value)}
+            >
+            
+            </input>
         </div>
         <div className='form-control'>
             <label>Set Reminder</label>
